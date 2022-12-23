d85a81e7514731c6f5d6519d9f030d362c992e13
NaveenCdp <naveen@cdp360.com>
2022-12-23 19:53:31 +0530

    change icon

--- src/components/partials/pro/headerstyle/header-pro.js
+++ src/components/partials/pro/headerstyle/header-pro.js
@@ -20,12 +20,6 @@ import CustomToggle from "../../../dropdowns";
 import Card from "../../../bootstrap/card";
 // import Modalpopup from "../../../../views/modules/auth/components/modal-popup";
 
-//img
-import img1 from "../../../../assets/images/shapes/01.png";
-import img2 from "../../../../assets/images/shapes/02.png";
-import img3 from "../../../../assets/images/shapes/03.png";
-import img4 from "../../../../assets/images/shapes/04.png";
-
 // logo
 import Logo from "../../components/logo";
 
@@ -38,6 +32,9 @@ import * as SettingSelector from "../../../../store/setting/selectors";
 import RadioBtn from "../../../setting/elements/radio-btn";
 import loginServices from "../../../../services/login_services/login-services";
 import { toast } from "react-toastify";
+import "./styles.scss";
+
+import { ReactComponent as Logout } from "../../../../assets/svg/logout.svg";
 
 const Headerpro = memo((props) => {
   let history = useNavigate();
@@ -267,24 +264,20 @@ const Headerpro = memo((props) => {
                     role="button"
                     aria-controls="elementsData"
                   >
+                    {/* <Logout className="svg-width" /> */}
                     <svg
-                      className="icon-20"
                       width="20"
                       viewBox="0 0 24 24"
-                      fill="currentColor"
+                      fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
+                      {" "}
                       <path
-                        opacity="0.4"
-                        d="M11.9912 18.6215L5.49945 21.8641C5.00921 22.1302 4.39768 21.9525 4.12348 21.4643C4.0434 21.3108 4.00106 21.1402 4 20.9668V13.7088C4 14.4284 4.40573 14.8726 5.47299 15.3701L11.9912 18.6215Z"
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M9.89535 11.23C9.45785 11.23 9.11192 11.57 9.11192 12C9.11192 12.42 9.45785 12.77 9.89535 12.77H16V17.55C16 20 13.9753 22 11.4724 22H6.51744C4.02471 22 2 20.01 2 17.56V6.45C2 3.99 4.03488 2 6.52762 2H11.4927C13.9753 2 16 3.99 16 6.44V11.23H9.89535ZM19.6302 8.5402L22.5502 11.4502C22.7002 11.6002 22.7802 11.7902 22.7802 12.0002C22.7802 12.2002 22.7002 12.4002 22.5502 12.5402L19.6302 15.4502C19.4802 15.6002 19.2802 15.6802 19.0902 15.6802C18.8902 15.6802 18.6902 15.6002 18.5402 15.4502C18.2402 15.1502 18.2402 14.6602 18.5402 14.3602L20.1402 12.7702H16.0002V11.2302H20.1402L18.5402 9.6402C18.2402 9.3402 18.2402 8.8502 18.5402 8.5502C18.8402 8.2402 19.3302 8.2402 19.6302 8.5402Z"
                         fill="currentColor"
-                      />
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M8.89526 2H15.0695C17.7773 2 19.9735 3.06605 20 5.79337V20.9668C19.9989 21.1374 19.9565 21.3051 19.8765 21.4554C19.7479 21.7007 19.5259 21.8827 19.2615 21.9598C18.997 22.0368 18.7128 22.0023 18.4741 21.8641L11.9912 18.6215L5.47299 15.3701C4.40573 14.8726 4 14.4284 4 13.7088V5.79337C4 3.06605 6.19625 2 8.89526 2ZM8.22492 9.62227H15.7486C16.1822 9.62227 16.5336 9.26828 16.5336 8.83162C16.5336 8.39495 16.1822 8.04096 15.7486 8.04096H8.22492C7.79137 8.04096 7.43991 8.39495 7.43991 8.83162C7.43991 9.26828 7.79137 9.62227 8.22492 9.62227Z"
-                        fill="currentColor"
-                      />
+                      ></path>{" "}
                     </svg>
                     <span className="nav-text ms-2">Logout</span>
                   </Nav.Link>
